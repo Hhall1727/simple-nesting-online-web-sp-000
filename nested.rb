40dfd3a075aa2f9a72ca81c05acc172bff708e2f
Halie Hall <halierae17@outlook.com>
2021-04-23 20:10:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,7 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash[:alan_kay][:known_for] 
 end
 
 def dennis_ritchies_language
@@ -104,7 +104,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+  programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
